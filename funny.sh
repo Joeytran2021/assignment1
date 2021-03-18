f9b247f67e3b71d376633e1ed97cc357ee949bb0
Joeytran2021 <79957926+Joeytran2021@users.noreply.github.com>
2021-03-19 04:38:25 +0700

    Update funny.sh

--- funny.sh
+++ funny.sh
@@ -5,20 +5,4 @@ echo "This is NOT funny"
 else
 echo "This is funny"
 fi
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-"funny.sh" 7L, 76C                                            7,2           All
+
