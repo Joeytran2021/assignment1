304c858fa564d63860c5fd1f80487e35cbf7a327
Joeytran2021 <79957926+Joeytran2021@users.noreply.github.com>
2021-03-19 04:26:40 +0700

    Create funny.sh

---
+++ funny.sh
@@ -0,0 +1,24 @@
+if [ $# -eq 0 ]
+then
+echo "This is NOT funny"
+
+else
+echo "This is funny"
+fi
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+"funny.sh" 7L, 76C                                            7,2           All
